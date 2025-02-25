9ded8bb571189f7f75a38b8781cf45928e8eef4d
appledog <appledog2k@gmail.com>
2025-02-25 10:53:27 +0700

    thêm logic agrregate

--- src/main/java/appledog/research/application/DataFlowStreamWordCount.java
+++ src/main/java/appledog/research/application/DataFlowStreamWordCount.java
@@ -1,26 +1,30 @@
 package appledog.research.application;
 
-import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.KGroupedStream;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
 
+import java.util.Arrays;
 import java.util.Properties;
 
-public class DataFlowStream {
+public class DataFlowStreamWordCount {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "data-flow-stream");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, String> stream = builder.stream("streams-dataflow-input");
-        stream.foreach((k, v) -> System.out.println("key: " + k + ", value: " + v));
-        stream.filter((k, v) -> v.contains("token"))
-                .map((k, v) -> KeyValue.pair(k, v.toUpperCase()))
-                .to("streams-dataflow-output");
+        KStream<String, String> stream = builder.stream("streams-wordcount-input");
+        KGroupedStream<String, String> kGroupedStream = stream.flatMapValues(v -> Arrays.asList(v.toLowerCase().split(" ")))
+                .groupBy((k, v) -> v);
+        KTable<String, Long> countTable = kGroupedStream.count();
+        countTable.toStream().to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
 
         Topology topology = builder.build();
         System.out.println(topology.describe());
